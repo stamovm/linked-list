781d43dee668f3fad6a974361a862c6120b5087d
stamovm <m.stamov@yahoo.com>
2021-10-04 15:30:40 -0700

    initial commit

---
+++ LinkedList.js
@@ -0,0 +1,20 @@
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.length = 0
+  }
+  insertAtHead(data) {
+    const newNode = new LinkedListNode(data, this.head)
+    this.head = newNode
+    length++
+  }
+}
+
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value
+    this.next = next
+  }
+}
+
+module.exports = LinkedList
