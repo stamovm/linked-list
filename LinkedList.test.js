f92bfa7a87e820d487696bd5f447c460c55a0a17
stamovm <m.stamov@yahoo.com>
2021-10-04 21:10:26 -0700

    updated the link list and fixed bugs

--- LinkedList.test.js
+++ LinkedList.test.js
@@ -16,7 +16,40 @@ describe('#insertAtHead', () => {
 describe('#getByIndex', () => {
   describe('with index less than 0', () => {
     test('Returns null', () => {
-      const ll = new LinkedList()
+      const ll = LinkedList.fromValues(10, 20)
+      expect(ll.getByIndex(-1)).toBeNull()
     })
+  })
+})
+
+describe('with index greater than index length', () => {
+  test('Returns null', () => {
+    const ll = LinkedList.fromValues(10, 20)
+    expect(ll.getByIndex(5)).toBeNull()
+  })
+})
+
+describe('with index 0', () => {
+  test('Returns the head of the list', () => {
+    const ll = LinkedList.fromValues(10, 20)
+    expect(ll.getByIndex(0).value).toBe(10)
+  })
+})
+
+describe('with index 0', () => {
+  test('Returns the element at that index', () => {
+    const ll = LinkedList.fromValues(10, 20, 30, 40, 50)
+    expect(ll.getByIndex(2).value).toBe(30)
+  })
+})
+describe('with index in the middle', () => {
+  test('remove at the given index', () => {
+    const ll = LinkedList.fromValues(10, 20, 30, 40)
+    ll.removeAtIndex(2)
+    const node = ll.getByIndex(1)
+
+    expect(node.value).toBe(20)
+    expect(node.next.value).toBe(40)
+    expect(ll.length).toBe(3)
   })
 })
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const LinkedList = require('./LinkedList')
 
-const ll = new LinkedList()
-ll.insertAtHead(10)
-ll.insertAtHead(20)
+const ll = LinkedList.fromValues(10, 20, 30, 40, 50)
 
-console.log(ll)
+ll.print()
+console.log(ll.getByIndex(2).value)
+ll.removeAtIndex(2)
+ll.print()
